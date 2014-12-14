04e1a4c0094e16e6420a70079d71fd5929428793
won <wjin@nyit.edu>
2014-12-13 23:05:20 -0500

    integration tests, partials custom routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if base_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
