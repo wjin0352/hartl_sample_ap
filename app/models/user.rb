42205e992ee0a4479de59966ddd395848cb8a2b1
won <wjin@nyit.edu>
2014-12-22 16:26:43 -0500

    reviewing last times changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # attr_accessor :name, :email
+
+  # call back before_save to downcase the emails Ensuring email uniqueness by downcasing
+  # the email attribute before being saved to database
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
